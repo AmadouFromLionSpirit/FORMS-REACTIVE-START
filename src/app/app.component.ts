b6bb010eb0d21cb9cedad0ff46fe143b235e6086
Sarr <contact@amadousarr.com>
2023-01-19 11:36:53 +0100

    Reactive : Grouping Controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,13 @@ export class AppComponent {
 
   ngOnInit() {
     this.signupForm = new FormGroup({
-      'username': new FormControl(null, Validators.required),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'userData': new FormGroup ( {
+        'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email])
+      
+      }),
       'gender': new FormControl('male')
+      
     });
   }
 
